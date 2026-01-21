2a315332934f68bdd08cff57a6f0f0b7b38f5b88
jrodrigo887 <jrodrigo887@gmail.com>
2026-01-20 22:26:31 -0300

    feat: update action buttons to link to Chá de Casa Nova page and comment out RSVP link

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -19,11 +19,17 @@
 
       <!-- Action Buttons -->
       <div class="buttons-container">
-        <router-link to="/confirmar-presenca" class="btn btn-gold">
+        <!-- <router-link to="/confirmar-presenca" class="btn btn-gold">
           <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
           </svg>
           Confirmar Presença
+        </router-link> -->
+        <router-link to="/cha-de-casa-nova" class="btn btn-gold">
+          <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+          </svg>
+          Chá de casa nova
         </router-link>
 
         <button @click="showPixModal = true" class="btn btn-emerald">
