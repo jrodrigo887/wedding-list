fdab5f0ece1cfaafd9f55a92b63734914c4c26da
jrodrigo887 <jrodrigo887@gmail.com>
2026-01-20 22:21:54 -0300

    feat: add Cha Casa Nova page with WhatsApp confirmation functionality and associated image

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // Views
 import HomePage from '@/views/HomePage.vue'
 import RsvpPage from '@/views/RsvpPage.vue'
+import ChaCasaNovaPage from '@/views/ChaCasaNovaPage.vue'
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     path: '/confirmar-presenca',
     name: 'rsvp',
     component: RsvpPage,
+  },
+  {
+    path: '/cha-de-casa-nova',
+    name: 'cha-casa-nova',
+    component: ChaCasaNovaPage,
   },
   // Redirect para pagina inicial se rota nao existir
   {
